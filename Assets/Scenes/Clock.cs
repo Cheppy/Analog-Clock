ed656ceb3cbbf4b34038ce11bf5dafcebc7e280f
Nick <teamasterln@gmail.com>
2019-04-26 22:40:37 +0300

    :tada:

---
+++ Assets/Scenes/Clock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	const float
+		degreesPerHour = 30f,
+		degreesPerMinute = 6f,
+		degreesPerSecond = 6f;
+
+    [SerializeField] private Transform transform_HoursArm;
+    [SerializeField] private Transform transform_MinutesArm;
+    [SerializeField] private Transform transform_SecondsArm;
+
+
+
+    void UpdateContinuous()
+    {
+
+
+    }
+}
---
+++ Assets/Scripts/ClockController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClockController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [SerializeField] private Transform HoursArm;
+    [SerializeField] private Transform MinutesArm;
+    [SerializeField] private Transform SecondsArm;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HoursArm.localRotation =
+              Quaternion.Euler(0f, DateTime.Now.Hour * 30f, 0f);
+        MinutesArm.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Minute * 6f, 0f);
+        SecondsArm.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Second * 6f, 0f);
+
+
+
+    }
+}
